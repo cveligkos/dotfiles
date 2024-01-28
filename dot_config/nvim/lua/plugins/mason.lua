2f0184838c9d0467a41986a718c705fed14bee6c
Christos Veligkos <cveligkos@gmail.com>
2024-01-28 13:03:33 +0200

    Update neovim config

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,46 @@
 return {
-    "williamboman/mason.nvim",
-    config = function()
-        require("mason").setup()
-    end
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+
+		mason.setup({})
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				-- language servers
+				"lua_ls",
+				"rust_analyzer",
+				"intelephense",
+				"clangd",
+			},
+
+			automatic_installation = true,
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				-- language servers
+				"lua_ls",
+				"rust_analyzer",
+				"intelephense",
+				"clangd",
+
+				-- formatters
+				"stylua",
+				"prettierd",
+				"isort",
+				"black",
+
+				--linters
+				"eslint_d",
+				"selene",
+			},
+		})
+	end,
 }
